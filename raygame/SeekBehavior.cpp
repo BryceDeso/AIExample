013f071405e52901f8c6605ca83a67f2b1359142
Bryce <brycedeso123@gmail.com>
2021-03-12 11:59:55 -0600

    added wanderAngle to WanderBehavior

--- raygame/SeekBehavior.cpp
+++ raygame/SeekBehavior.cpp
@@ -16,7 +16,7 @@ SeekBehavior::SeekBehavior(Actor* target, float seekForce)
 
 MathLibrary::Vector2 SeekBehavior::calculateForce(Agent* agent)
 {
-	//Find the direction ot move in.
+	//Find the direction to move in.
 	MathLibrary::Vector2 direction = MathLibrary::Vector2::normalize(m_target->getWorldPosition() - agent->getWorldPosition());
 
 	//Scale the direction vector by seekForce
--- raygame/WanderBehavior.cpp
+++ raygame/WanderBehavior.cpp
@@ -1,5 +1,4 @@
 #include "WanderBehavior.h"
-#include "SeekBehavior.h"
 #include "Agent.h"
 #include "Actor.h"
 #include "raylib.h"
@@ -20,23 +19,24 @@ MathLibrary::Vector2 WanderBehavior::calculateForce(Agent* agent)
 {
 	//Initalize floats
 	float circleDistance= 3;
-	float circleRadius = 2;
+	float circleRadius = 6;
 	float wanderAngle = 20;
 
 	//Initalize vector2s
 	MathLibrary::Vector2 circleCenter;
 	MathLibrary::Vector2 circleDisplacement;
 	MathLibrary::Vector2 direction;
-	MathLibrary::Vector2 target;
+	MathLibrary::Vector2 desiredVelocity;
+	MathLibrary::Vector2 steeringForce;
 
 	//Get the circle's center by getting the agent's forward then scaling it by the desired distance to put the circle.
 	circleCenter = agent->getForward() * circleDistance;
+	//Offset the angle to make sure to get a diffrent angle everytime.
+	wanderAngle += (rand() * wanderAngle) - (wanderAngle * .5f);
 	//Get a random direction for the agent to move in.
-	direction = MathLibrary::Vector2(cos(rand()), sin(rand()));
+	direction = MathLibrary::Vector2(cos(wanderAngle), sin(wanderAngle));
 	//Get the circle's displacement by adding the circle's center by direction multiplied by the circles radius.
 	circleDisplacement = circleCenter + (direction * circleRadius);
-	//Sets the agent's target to be the vector and apply seek to move to the point.
-	
 
 	return circleDisplacement;
 }
